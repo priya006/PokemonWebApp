6bf8c70b47882eb1fd9b8e2bf30b43be550b6c93
Priya Boopathi <priyanandh06@gmail.com>
2024-06-11 23:43:41 -0700

    Added a https://leetcode.com/problems/longest-substring-without-repeating-characters/ problem to my lib

---
+++ src/test/kotlin/com/pokemonbackend/demo/leetcode/LongestSubstringWithoutRepeatingCharacters.kt
@@ -0,0 +1,25 @@
+package com.pokemonbackend.demo.leetcode
+
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
+class LongestSubstringWithoutRepeatingCharacters {
+    fun lengthOfLongestSubstring(s: String): Int {
+        var hashSet = HashSet<Char>()
+        var max = 0 // Keeping track of the maximum length of the unique substrings
+        var a_pointer = 0 //move around
+        var b_pointer = 0
+
+        while(a_pointer < s.length){
+            if(!hashSet.contains(s[a_pointer])){
+                hashSet.add(s[a_pointer])
+                max = Math.max(hashSet.size,max)
+                a_pointer ++
+            }else{
+                hashSet.remove(s[b_pointer])
+                b_pointer ++
+            }
+        }
+
+        return max
+
+    }
+}
