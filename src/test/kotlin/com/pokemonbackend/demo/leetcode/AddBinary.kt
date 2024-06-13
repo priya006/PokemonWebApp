60eabe932e7c6e393126047e50d1a0657ff2904c
Priya Boopathi <priyanandh06@gmail.com>
2024-06-13 10:52:41 -0700

    fixed index out of bound exception

--- src/test/kotlin/com/pokemonbackend/demo/leetcode/AddBinary.kt
+++ src/test/kotlin/com/pokemonbackend/demo/leetcode/AddBinary.kt
@@ -1,29 +1,35 @@
 package com.pokemonbackend.demo.leetcode
 
 //https://leetcode.com/problems/add-binary/description/
-class AddBinary {
-
-    fun addTheBinaries(a : String, b :String):String{
+class Solution {
+    fun addBinary(a: String, b: String): String {
+        //sum is appended to the resulting string builder
         var stringBuilder = StringBuilder()
         var i = a.length-1
         var j = b.length-1
         var carry = 0
 
-        while(i>= 0 || j >=0){
+        while(i>= 0 || j>= 0){
             var sum = carry
-            sum += a[i--] - '0'
-            sum += b[j--] - '0'
+            if(i>=0){
+                sum += a[i] - '0'
+                i--
+            }
+
+
+            if(j>= 0){
+                sum += b[j] - '0'
+                j--
+            }
+
             stringBuilder.append(sum%2)
             carry = sum/2
-
         }
-        if(carry != 0) stringBuilder.append(carry)
 
+        if(carry!= 0 ) stringBuilder.append(carry)
         return stringBuilder.reverse().toString()
+
     }
-
-
-
 }
 
 fun main(){
