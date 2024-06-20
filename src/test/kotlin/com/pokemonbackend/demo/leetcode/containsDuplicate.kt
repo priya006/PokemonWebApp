7612cdb67424494f10584b07c44f357ef2993a14
Priya Boopathi <priyanandh06@gmail.com>
2024-06-20 16:04:34 -0700

    solved the problem https://leetcode.com/problems/contains-duplicate/

---
+++ src/test/kotlin/com/pokemonbackend/demo/leetcode/containsDuplicate.kt
@@ -0,0 +1,15 @@
+package com.pokemonbackend.demo.leetcode
+
+class containsDuplicate {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        var hashSet = hashSetOf<Int>()
+        for(i in nums.indices){
+            if(hashSet.contains(nums[i])){
+                return true
+            }else{
+                hashSet.add(nums[i])
+            }
+        }
+        return false
+    }
+}
