a569845bfa658444dc6c015640088ecbbbdab966
Priya Boopathi <priyanandh06@gmail.com>
2024-06-26 23:58:32 -0700

    Added the code for MajorityElement

---
+++ src/test/kotlin/com/pokemonbackend/demo/leetcode/majorityelement.kt
@@ -0,0 +1,23 @@
+package com.pokemonbackend.demo.leetcode
+
+class MajorityElement {
+    fun majorityElement(nums: IntArray): Int {
+        var count = 0
+        var result = 0
+
+        for(i in nums.indices){
+
+            if(count == 0){
+                result = nums[i]
+            }else{
+                count += if(result == nums[i]) 1 else -1
+            }
+
+        }
+        return result
+    }
+}
+
+//Hint:Boyers-moyers algorithm.Keep the result and the count.If you see the number in the "count" becoming
+//zero then change the result being the current element in array
+//else increase the count when result == element from array else decrement the count 
