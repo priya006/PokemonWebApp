392196934835bfe3ce2746c3751b2ad3cd45cdd6
Priya Boopathi <priyanandh06@gmail.com>
2024-06-30 13:29:01 -0700

    Added reorderLinked list

---
+++ src/test/kotlin/com/pokemonbackend/demo/leetcode/LinkedList/4.removeNthFromEnd.kt
@@ -0,0 +1,28 @@
+package com.pokemonbackend.demo.leetcode.LinkedList
+
+class Solution {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        var dummy = ListNode(0)
+        //connecting dummy to the head
+        dummy.next = head
+        var slowPointer:ListNode? = dummy
+        var fastPointer:ListNode? = dummy
+
+        //important to use until. So that means fast pointer is moving n times ahead
+        for(i in 0 until n){
+            fastPointer = fastPointer?.next
+        }
+
+        while(fastPointer?.next != null){
+            //move both the pointers
+            slowPointer = slowPointer?.next
+            fastPointer = fastPointer?.next
+        }
+
+        slowPointer?.next = slowPointer?.next?.next
+        return dummy.next
+    }
+}
+//Hint: Move the fast pointer n times head of the slow pointer.
+//When the fast pointer.next = null then link the slowpointer next to slowpointer.next.next
+
---
+++ src/test/kotlin/com/pokemonbackend/demo/leetcode/LinkedList/5.ReorderList.kt
@@ -0,0 +1,45 @@
+package com.pokemonbackend.demo.leetcode.LinkedList
+
+class ReorderList {
+    fun reorderList(head: ListNode?) {
+        //Splitting into two linked list using slow and fast pointer
+        var slowPointer:ListNode? = head
+        var fastPointer:ListNode? = head
+
+        while(fastPointer?.next != null || fastPointer?.next?.next != null){
+            slowPointer = slowPointer?.next
+            fastPointer = fastPointer?.next?.next
+        }
+
+        //Split into two linked List
+        var h2:ListNode? = slowPointer?.next
+        var h1:ListNode? = head
+        slowPointer?.next = null //detatching the first linked list
+
+        //reverse the h2 linked list
+        var current:ListNode?= h2
+        var previous:ListNode? = null
+
+        while(current != null){
+            val tempNext = current.next
+            current.next = previous
+            previous = current
+            current = tempNext
+        }
+        //return the reverse linked list
+        h2 = previous
+
+        while(h1 != null && h2 != null){
+            val nextTemp = h1.next
+            h1.next = h2
+
+            h1 = h2
+            h2 = nextTemp
+        }
+
+    }
+}
+
+//Hint:1. Split the linked list into two linked list using slow and fast pointer (hare and tortoise)
+//2.Now reverse the second linked list
+//3.Now merge the two linked list 
